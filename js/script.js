e0f7ecfbcc8d4c6bf557c9fd9ef54243c0630b10
samuelecozzubbo <samuele.cozzubbo@gmail.com>
2024-06-12 15:19:53 +0200

    Build: create Palindromeword check function

--- js/script.js
+++ js/script.js
@@ -1,17 +1,28 @@
 //parola palindroma
 
-let parola ="ciao";
+let parola = prompt("Inserisci una parola");
 let parolaInversa="";
 
 //Creo una funzione inverti parola
 function invertiParola(parola) {
     for(let i = parola.length - 1; i >= 0; i--){
         parolaInversa += parola[i];
-        console.log(parolaInversa);
     }
     return parolaInversa;
 }
 
+//Funzione confronto parole palindrome
+function palindromeCheck(parolaInversa){
+    if(parolaInversa === parola){
+        return "parola palindroma";
+    } else {
+        return "parola non palindroma";
+    }
+}
+//stampa risultato funzione inverti parola
 parolaInversa = invertiParola(parola);
 console.log("La parola invertita è:",parolaInversa);
+//Stampa risultato funzione check palindroma
+let palindroma = palindromeCheck(parolaInversa);
+console.log(palindroma);
 
