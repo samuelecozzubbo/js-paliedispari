5d53d751e55eb1525ee9aaf1c7b0b81612fdb113
samuelecozzubbo <samuele.cozzubbo@gmail.com>
2024-06-12 15:38:21 +0200

    Feat: add sum function

--- pariodispari/js/ParieDispari.js
+++ pariodispari/js/ParieDispari.js
@@ -1,13 +1,23 @@
-let scelta = "pari";
 
 //funzione che genera un numero random 
-    //Funzione
-    function RandomNumberGenerator(min, max) {
-        let randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
-        return randomNumber; 
-    }
+//Funzione
+function RandomNumberGenerator(min, max) {
+    let randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+    return randomNumber; 
+}
 
-//Invocazione
-let userNumber = RandomNumberGenerator(0,5);
+function sum (num1, num2){
+    let sumResult = num1 + num2;
+    return sumResult;
+}
+
+//scelta utente
+let scelta;
+let userNumber = 3;
+
+//Invocazione funzione randomGenerator
 let pcNumber =  RandomNumberGenerator(0,5);
 console.log(userNumber, pcNumber);
+//Invocazione somma
+let sumNumber = sum(userNumber, pcNumber);
+console.log(sumNumber);
